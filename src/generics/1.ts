ea15065fde4e454921336ec8399901a54e582971
Volodymyr Shkimba <shkimbavova@gmail.com>
2024-11-16 21:06:05 +0200

    fix 1.ts: second check

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-async function fetchData(url: string): Promise<object> {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
